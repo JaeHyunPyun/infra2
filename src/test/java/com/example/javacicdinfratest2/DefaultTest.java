dde997caf8ea7145ed3d9f1eb2e58433fa2cb447
jaehyunpyun <pyunjaehyun@gmail.com>
2025-02-23 12:50:54 +0900

    feat:  cicd pipeline add

---
+++ src/test/java/com/example/javacicdinfratest2/DefaultTest.java
@@ -0,0 +1,9 @@
+package com.example.javacicdinfratest2;
+
+public class DefaultTest {
+//    @Test
+//    void testAlwaysFail() {
+//        // 실패하는 조건을 만들기 위해 거짓 값을 assert
+//        assertTrue(false, "This test will always fail");
+//    }
+}
