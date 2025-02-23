56bb2d740066a12dfcaa62c801b8d34291514a90
jaehyunpyun <pyunjaehyun@gmail.com>
2025-02-23 12:35:29 +0900

    first commit

---
+++ src/main/java/com/example/javacicdinfratest2/Javacicdinfratest2Application.java
@@ -0,0 +1,13 @@
+package com.example.javacicdinfratest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javacicdinfratest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javacicdinfratest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javacicdinfratest2/Javacicdinfratest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacicdinfratest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javacicdinfratest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
